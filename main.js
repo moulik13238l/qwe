414d4f9ff6a95af41ce713b2534204d693d2af60
moulik13238l <86113949+moulik13238l@users.noreply.github.com>
2022-02-04 19:31:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size (550, 500);
+   
+    canvas = createCanvas(550 , 550);
+    canvas.position(560 , 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    background('#808080');
+}
+
+function modelLoaded() {
+    console.log('POSENET IS INITIALIZED ! ');
+}
+
+function gotPoses() {
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
